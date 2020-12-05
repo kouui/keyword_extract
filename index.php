71b8a69d842ef937e3ab01a1b31be41e2ec439b1
kouui <lelouchkako@gmail.com>
2020-12-05 01:37:03 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>NEWS VIEW</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="statics/css/bootstrap.min.css">
+  <script src="statics/js/jquery.min.js"></script>
+  <script src="statics/js/popper.min.js"></script>
+  <script src="statics/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Title of this website</h1>
+  <p>Some description here!</p>
+</div>
+
+<div class="container">
+  <div class="card">
+    <div class="card-body">
+      <div class="col">
+
+        <div class="divider bg-dark"><hr></div>
+
+        <h5>このニュースから銘柄を探す</h5>
+
+        <div class="divider"><hr></div>
+
+        <p> 09/25 20:21 : アパレル不況でもユニクロ絶好調。猛暑で8月売上高が「前年比3割増」に </p>
+
+        <p class="text-right">
+          <span class="font-weight-bold"><a href=#>このニュースを見る > </a></span>
+        </p>
+
+        <div class="divider"><hr></div>
+
+
+        <p>
+          <span class="font-weight-bold"> 関連ワード :</span>
+          &nbsp &nbsp
+          <button type="button" class="btn btn-secondary">
+            エアリズム
+          </button>
+          &nbsp
+          <button type="button" class="btn btn-secondary">
+            マスク
+          </button>
+          &nbsp
+          <button type="button" class="btn btn-secondary">
+            コロナ
+          </button>
+          &nbsp
+          <button type="button" class="btn btn-secondary">
+            繊維
+          </button>
+        </p>
+
+        <div class="divider bg-dark"><hr></div>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
